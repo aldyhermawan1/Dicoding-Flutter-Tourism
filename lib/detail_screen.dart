84eec1736963e7d723a572c00e66e82257c06e1a
Aldy Hermawan <aldyjonkunimen@gmail.com>
2023-01-05 18:48:48 +0800

    Add list screen and adjust detail screen

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:tourism_app/model/tourism_place.dart';
 
 var informationTextStyle = const TextStyle(fontFamily: 'Oxygen');
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
+  final TourismPlace place;
+
+  const DetailScreen({Key? key, required this.place}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,13 +15,43 @@ class DetailScreen extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Image.asset('images/farm-house.jpg'),
+          Stack(
+            children: [
+              Hero(
+                tag: place.imageAsset,
+                child: Image.asset(place.imageAsset),
+              ),
+              SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.grey,
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.arrow_back,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ),
+                      const FavoriteButton(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
           Container(
             margin: const EdgeInsets.only(top: 16),
-            child: const Text(
-              'Farm House Lembang',
+            child: Text(
+              place.name,
               textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 30, fontFamily: 'Staatliches'),
+              style: const TextStyle(fontSize: 30, fontFamily: 'Staatliches'),
             ),
           ),
           Container(
@@ -31,7 +64,7 @@ class DetailScreen extends StatelessWidget {
                     const Icon(Icons.calendar_today),
                     const SizedBox(height: 8),
                     Text(
-                      'Open Everyday',
+                      place.openDays,
                       style: informationTextStyle,
                     ),
                   ],
@@ -41,7 +74,7 @@ class DetailScreen extends StatelessWidget {
                     const Icon(Icons.access_time),
                     const SizedBox(height: 8),
                     Text(
-                      '09.00 - 20.00',
+                      place.openTime,
                       style: informationTextStyle,
                     ),
                   ],
@@ -51,7 +84,7 @@ class DetailScreen extends StatelessWidget {
                     const Icon(Icons.monetization_on),
                     const SizedBox(height: 8),
                     Text(
-                      'RP 25.000',
+                      place.ticketPrice,
                       style: informationTextStyle,
                     ),
                   ],
@@ -61,46 +94,57 @@ class DetailScreen extends StatelessWidget {
           ),
           Container(
             padding: const EdgeInsets.all(16),
-            child: const Text(
-              'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+            child: Text(
+              place.description,
               textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16),
+              style: const TextStyle(fontSize: 16),
             ),
           ),
           SizedBox(
             height: 150,
             child: ListView(
               scrollDirection: Axis.horizontal,
-              children: [
-                Padding(
+              children: place.imageUrls.map((url) {
+                return Padding(
                   padding: const EdgeInsets.all(4),
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(4),
-                    child: Image.network(
-                        'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
+                    child: Image.network(url),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(4),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(4),
-                    child: Image.network(
-                        'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(4),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(4),
-                    child: Image.network(
-                        'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
-                  ),
-                ),
-              ],
+                );
+              }).toList(),
             ),
           )
         ],
       ),
     ));
+  }
+}
+
+class FavoriteButton extends StatefulWidget {
+  const FavoriteButton({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FavoriteButtonState();
+  }
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        isFavorite ? Icons.favorite : Icons.favorite_border,
+        color: Colors.red,
+      ),
+      onPressed: () {
+        setState(() {
+          isFavorite = !isFavorite;
+        });
+      },
+    );
   }
 }
